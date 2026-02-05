ef6b9123da031df6e3799ab6eb19b00fa2bfa7cf
Teena Devray <teenadevray24@gmail.com>
2026-02-05 13:02:11 +0530

    add root route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,10 @@ app.use('/api', authRoutes);
 connectDB();
 
 const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("API is running successfully 🚀");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
