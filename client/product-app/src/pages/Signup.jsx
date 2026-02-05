612ede46bca60f56a4ecaa54e148c1515c416a13
Teena Devray <teenadevray24@gmail.com>
2026-02-05 13:50:21 +0530

    fetchapi

--- client/product-app/src/pages/Signup.jsx
+++ client/product-app/src/pages/Signup.jsx
@@ -9,14 +9,15 @@ const Signup = () => {
 const handleSignup = async (e) => {
   e.preventDefault();
 
-  const res = await fetch("http://localhost:3000/api/register", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      username,
-      password,
-    }),
-  });
+const res = await fetch(`${import.meta.env.VITE_API_URL}/api/register`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    username,
+    password,
+  }),
+});
+
 
   const data = await res.json();
 
