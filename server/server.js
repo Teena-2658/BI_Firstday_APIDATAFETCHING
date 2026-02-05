cdcbb5553c8ef5601f484667edecd3e3ace83e5e
Teena Devray <teenadevray24@gmail.com>
2026-02-05 15:30:18 +0530

    serverfile update

--- server/server.js
+++ server/server.js
@@ -1,36 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const { connectDB } = require('./common/db/mongo');
-const authRoutes = require('./modules/auth/auth.routes');
-
-const app = express();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://bi-firstday-apidatafetching.vercel.app"
+];
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "https://bi-firstday-apidatafetching.vercel.app"
-  ],
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, origin); // 👈 return exact origin
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-
-app.use(express.json());
-
-// routes
-app.use('/api', authRoutes);
-
-// db
-connectDB();
-
-const PORT = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-  res.send("API is running successfully 🚀");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
+// 🔴 THIS LINE IS CRITICAL
+app.options("*", cors());
