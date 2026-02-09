96bbb091cf02e12c43023a22c8b720530291e71a
Teena Devray <teenadevray24@gmail.com>
2026-02-09 15:14:06 +0530

    server cors with url

--- server/server.js
+++ server/server.js
@@ -18,9 +18,9 @@ if (!fs.existsSync("uploads")) {
 /* -------------------- CORS (OPEN – FINAL FIX) -------------------- */
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    // allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
